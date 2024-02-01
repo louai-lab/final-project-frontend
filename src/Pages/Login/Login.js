acffef8dc1b2a18859f8ca0849132fae34bb409d
louai <louaibaghdadi27@gmail.com>
2024-02-01 22:06:57 +0200

    login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,39 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import StyleLogin from "./Login.module.css";
-import football from "../../Assets/icons/soccer-ball-variant.png";
 import logo from "../../Assets/icons/Lebanese_Football_Association_(LFA)_logo.svg";
-import whistle from "../../Assets/icons/whistle (1).png";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaGoogle } from "react-icons/fa";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <div className={StyleLogin.loginPage}>
       <section className={StyleLogin.firstSection}>
         <img src={logo} alt="logo" />
         <div className={StyleLogin.color}></div>
         <div className={StyleLogin.color}></div>
-        <div className={StyleLogin.color}></div>
         <div className={StyleLogin.box}>
-          <div className={StyleLogin.square} style={{ "--i": 1 }}>
-            <img src={football} alt="Football Icon 0" />
-          </div>
-          <div className={StyleLogin.square} style={{ "--i": 2 }}>
-            <img src={whistle} alt="whistle" />
-          </div>
-          <div className={StyleLogin.square} style={{ "--i": 3 }}></div>
-          <div className={StyleLogin.square} style={{ "--i": 4 }}></div>
-          <div className={StyleLogin.square} style={{ "--i": 5 }}></div>
           <div className={StyleLogin.container}>
             <div className={StyleLogin.form}>
               <h2>Login Form</h2>
               <form>
                 <div className={StyleLogin.inputBox}>
-                  <input type="text" placeholder="Username" />
+                  <input type="text" placeholder="Email" />
                 </div>
-                <div className={StyleLogin.inputBox}>
-                  <input type="password" placeholder="Password" />
+                <div
+                  className={StyleLogin.inputBox}
+                  style={{ display: "flex" }}
+                >
+                  {/* <input type="password" placeholder="Password" /> */}
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div
+                    className={StyleLogin.eyeIcon}
+                    onClick={handleTogglePassword}
+                  >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                  </div>
                 </div>
-                <div className={StyleLogin.inputBox}>
+                <div className={StyleLogin.inputBox} style={{display:"flex", justifyContent:"center",alignItems:"center"}}>
                   <input type="submit" value="Login" />
+                  <button className={StyleLogin.oauth}>
+                    <FaGoogle />
+                    Login with Google
+                  </button>
                 </div>
               </form>
             </div>
