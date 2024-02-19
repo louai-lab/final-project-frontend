8687c426838da171c3d1d5d6ea406190da98f330
louai <louaibaghdadi27@gmail.com>
2024-02-19 16:59:48 +0200

    match page

--- src/Pages/Matches/Matches.js
+++ src/Pages/Matches/Matches.js
@@ -2,16 +2,18 @@ import React from "react";
 import StyleMatches from "./Matches.module.css";
 import { useMatchesStore } from "../../Zustand/Store";
 import logo from "../../Assets/icons/Lebanese_Football_Association_(LFA)_logo.svg";
-import StadiumIcon from '@mui/icons-material/Stadium';
+import StadiumIcon from "@mui/icons-material/Stadium";
 
 function Matches() {
   const { lastMatch } = useMatchesStore();
+  const { matches } = useMatchesStore();
+  // console.log(matches)
   // console.log(lastMatch.team_a.team.image);
 
   return (
-    <div className={StyleMatches.matchesContainer}>
-      <div className={StyleMatches.matchesHeroSection}>
-        <div className={StyleMatches.lastMatch}>
+    <main className={StyleMatches.matchesContainer}>
+      <article className={StyleMatches.matchesHeroSection}>
+        <section className={StyleMatches.lastMatch}>
           <img
             src={logo}
             width={100}
@@ -19,8 +21,8 @@ function Matches() {
             alt="Lebanese Football Association"
           />
           <p>{lastMatch.title}</p>
-        </div>
-        <div className={StyleMatches.twoTeams}>
+        </section>
+        <section className={StyleMatches.twoTeams}>
           <img
             src={`${process.env.REACT_APP_IMAGE_PATH}/${lastMatch.team_a?.team.image}`}
             alt={lastMatch.team_a?.team.name}
@@ -35,13 +37,52 @@ function Matches() {
             alt={lastMatch.team_b?.team.name}
             className={StyleMatches.teamsImage}
           />
-        </div>
-        <div className={StyleMatches.stadium}>
-          <StadiumIcon/>
+        </section>
+        <section className={StyleMatches.stadium}>
+          <StadiumIcon />
           <p>{lastMatch.pitch}</p>
-        </div>
-      </div>
-    </div>
+        </section>
+      </article>
+
+      <article className={StyleMatches.middle}>
+        <h1>ALL Matches</h1>
+      </article>
+
+      <article className={StyleMatches.cardsContainer}>
+        {matches.map((match) => {
+          return (
+            <article key={match._id} className={StyleMatches.cardMatch}>
+              <section className={StyleMatches.cardImages}>
+                <img
+                  src={`${process.env.REACT_APP_IMAGE_PATH}/${match.team_a?.team.image}`}
+                  alt={match.team_a?.team.name}
+                  className={StyleMatches.cardImage}
+                />
+                <span>VS</span>
+                <img
+                  src={`${process.env.REACT_APP_IMAGE_PATH}/${match.team_b?.team.image}`}
+                  alt={match.team_b?.team.name}
+                  className={StyleMatches.cardImage}
+                />
+              </section>
+
+              <section className={StyleMatches.cardTitle}>
+                {match.title}
+              </section>
+
+              <section className={StyleMatches.oneTeamScore}>
+                <p>{match.team_a?.team.name}</p>
+                <p>{match.team_a?.score}</p>
+              </section>
+              <section className={StyleMatches.oneTeamScore}>
+                <p>{match.team_b?.team.name}</p>
+                <p>{match.team_b?.score}</p>
+              </section>
+            </article>
+          );
+        })}
+      </article>
+    </main>
   );
 }
 
